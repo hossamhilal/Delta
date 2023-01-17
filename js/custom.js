53df90602a43e0a75cc06f8402a4283fc12b303b
Hossam Hilal <46751584+hossamhilal@users.noreply.github.com>
2023-01-17 21:11:14 +0300

    header

--- js/custom.js
+++ js/custom.js
@@ -19,99 +19,63 @@
     // });
 
     // sticky navbar
-    let viewHeight = window.innerHeight;
-    $(window).on('scroll',function () {
-       $(window).scrollTop() > (viewHeight + 100) ? $('.navWrapper').addClass('sticky') : $('.navWrapper').removeClass('sticky');
-    });
+    // let viewHeight = window.innerHeight;
+    // $(window).on('scroll',function () {
+    //    $(window).scrollTop() > (viewHeight + 100) ? $('.navWrapper').addClass('sticky') : $('.navWrapper').removeClass('sticky');
+    // });
 
  
     // Scroll Menu
-    $('.navMenu a').on('click', function (e) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top - 100
-        }, 2000, 'linear');
-        $('.mainMenu li').removeClass('active');
-        $(this).parent().addClass('active');
-    });  
+    // $('.navMenu a').on('click', function (e) {
+    //     e.preventDefault();
+    //     $('html, body').animate({
+    //         scrollTop: $($(this).attr('href')).offset().top - 100
+    //     }, 2000, 'linear');
+    //     $('.mainMenu li').removeClass('active');
+    //     $(this).parent().addClass('active');
+    // });  
 
-    $(window).on('scroll',function () {
-        var sections = $('section')
-        sections.each(function() {
-            let top = window.scrollY ;
-            var secOffset = $(this).offset().top - 100,
-                bottom = top + $(this).outerHeight();
+    // $(window).on('scroll',function () {
+    //     var sections = $('section')
+    //     sections.each(function() {
+    //         let top = window.scrollY ;
+    //         var secOffset = $(this).offset().top - 100,
+    //             bottom = top + $(this).outerHeight();
             
-            if (top >= secOffset && top <= bottom) {
-                let activeId = $(this).attr('id');
-                $('.navMenu li').removeClass('active');
+    //         if (top >= secOffset && top <= bottom) {
+    //             let activeId = $(this).attr('id');
+    //             $('.navMenu li').removeClass('active');
                 
-                $('.navMenu a[href="#'+activeId+'"]').parent().addClass('active');
-            }
-        });
-    });
+    //             $('.navMenu a[href="#'+activeId+'"]').parent().addClass('active');
+    //         }
+    //     });
+    // });
     
-    // Go through a sentence, wrap its characters with spans
-    function setUpCharacters() {
-        var $sentences = $('.header h1');
-
-        // Run for each sentence
-        $sentences.each(function () {
-            var $sentence = $(this);
-            var newContent = '';
-
-            // Go through all characters of the sentence
-            var x = 0.1;
-            for (let i = 0; i < $sentence.text().length; i++) {
-                var substring = $sentence.text().substr(i, 1);
-
-                // If we have a character, wrap it
-                if (substring != " ") {
-                    newContent += '<span style="animation-delay:'+x+'s">' + substring + '</span>';
-                    x = x + 0.2;
-                } else {
-                    newContent += substring;
-                }
-            }
-
-            // Replace content
-            $sentence.html(newContent);
-        });
-    }
-    setUpCharacters();
-
-    setTimeout(function(){
-        $('.header p').addClass('show');
-        setTimeout(function() {
-            $('.header .flex').addClass('show');
-        }, 1000);
-    }, 10000);
-
 
     // COUNTER
-    let a = 0;
-    $(window).scroll(function() {
-        var oTop = $('.features').offset().top - window.innerHeight;
-        if (a == 0 && $(window).scrollTop() > oTop) {
-            count();
-        }
-    });
+    // let a = 0;
+    // $(window).scroll(function() {
+    //     var oTop = $('.features').offset().top - window.innerHeight;
+    //     if (a == 0 && $(window).scrollTop() > oTop) {
+    //         count();
+    //     }
+    // });
 
-    function count () {
-        $('.statistic h3').each(function () {
-            // let countNum = $(this).text();
-            $(this).prop('Counter',0).animate({
-                Counter: $(this).text()
-            }, {
-                duration: 7000,
-                easing: 'swing',
-                step: function (now) {
-                    $(this).text(Math.ceil(now));
-                }
-            });
-        });
-        a = 1;
-    }
+    // function count () {
+    //     $('.statistic h3').each(function () {
+    //         // let countNum = $(this).text();
+    //         $(this).prop('Counter',0).animate({
+    //             Counter: $(this).text()
+    //         }, {
+    //             duration: 7000,
+    //             easing: 'swing',
+    //             step: function (now) {
+    //                 $(this).text(Math.ceil(now));
+    //             }
+    //         });
+    //     });
+    //     a = 1;
+    // }
 
     // OPEN SIDE  MENU 
     $('.menuBtn').on('click', function () {
@@ -130,46 +94,34 @@
     });
 
     
-    // Partners Carousel 
-    let partnerOwl = $('.partnerOwl');
-    partnerOwl.owlCarousel({
+    // Header Carousel 
+    $('.owlHeader').owlCarousel({
         loop:true,
         margin:0,
         nav:false,
-        dots: false,
+        dots: true,
         responsive:{
             0:{
                 items:1
             },
             600:{
-                items:3
+                items:1
             },
             1000:{
-                items:4
+                items:1
             }
         }
     });
 
-    // Go to the previous item
-    $('.parntnerPrevious').click(function() {
-        partnerOwl.trigger('prev.owl.carousel');
-    });
-
-    // Go to the next item
-    $('.parntnerNext').click(function() {
-        partnerOwl.trigger('next.owl.carousel');
-    });
-
     // Team Carousel 
-    let teamOwl = $('.teamOwl');
-    teamOwl.owlCarousel({
+   $('.owlMaterials').owlCarousel({
         loop:true,
-        margin:0,
+        margin:20,
         nav:false,
         dots: false,
         responsive:{
             0:{
-                items:1
+                items:2
             },
             600:{
                 items:3
